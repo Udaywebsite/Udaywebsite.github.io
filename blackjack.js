f9ac18f10f6d4de0c8d47271f4185432eeaff484
Udaywebsite <128072491+Udaywebsite@users.noreply.github.com>
2023-03-17 15:40:04 -0500

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -1,6 +1,3 @@
-function openBlackjack() {
-  window.open("blackjack.html", "_blank", "width=600,height=400");
-}
 class Card {
   // ...
 }
@@ -25,7 +22,9 @@ function updateHandDisplay(handElement, hand) {
   handElement.innerHTML = hand.toString().replace(/,/g, '<br>');
 }
 
-
+function openBlackjack() {
+  window.open("blackjack.html", "_blank", "width=600,height=400");
+}
 
 function playBlackjack() {
   const deck = new Deck();
@@ -38,48 +37,4 @@ function playBlackjack() {
   playerHand.addCard(deck.drawCard());
   playerHand.addCard(deck.drawCard());
   dealerHand.addCard(deck.drawCard());
-  dealerHand.addCard(deck.drawCard());
-
-  updateHandDisplay(document.getElementById('player-hand'), playerHand);
-  updateHandDisplay(document.getElementById('dealer-hand'), dealerHand);
-
-  const hitButton = document.getElementById('hit');
-  const standButton = document.getElementById('stand');
-
-  hitButton.addEventListener('click', () => {
-    playerHand.addCard(deck.drawCard());
-    updateHandDisplay(document.getElementById('player-hand'), playerHand);
-
-    if (playerHand.getScore() > 21) {
-      document.getElementById('result').innerHTML = 'Bust! You lose.';
-      hitButton.disabled = true;
-      standButton.disabled = true;
-    }
-  });
-
-  standButton.addEventListener('click', () => {
-    while (dealerHand.getScore() < 17) {
-      dealerHand.addCard(deck.drawCard());
-    }
-    updateHandDisplay(document.getElementById('dealer-hand'), dealerHand);
-
-    const result = document.getElementById('result');
-
-    if (dealerHand.getScore() > 21) {
-      result.innerHTML = "Dealer busts! You win.";
-    } else if (playerHand.getScore() > dealerHand.getScore()) {
-      result.innerHTML = "You win!";
-    } else if (playerHand.getScore() < dealerHand.getScore()) {
-      result.innerHTML = "You lose.";
-    } else {
-      result.innerHTML = "It's a tie.";
-    }
-
-    hitButton.disabled = true;
-    standButton.disabled = true;
-  });
-
-  hitButton.disabled = false;
-  standButton.disabled = false;
-}
-
+  dealer

c11901ea71b39621a01c4cc7dda798a7c11e3c28
Udaywebsite <128072491+Udaywebsite@users.noreply.github.com>
2023-03-17 15:35:08 -0500

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -25,32 +25,7 @@ function updateHandDisplay(handElement, hand) {
   handElement.innerHTML = hand.toString().replace(/,/g, '<br>');
 }
 
-// Get the modal
-var modal = document.getElementById("blackjackModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("playBlackjack");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-  playBlackjack();
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 function playBlackjack() {
   const deck = new Deck();

1de3b9f96a362df80946cb4c6157dba2eaff34de
Udaywebsite <128072491+Udaywebsite@users.noreply.github.com>
2023-03-20 00:43:52 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,22 +1,79 @@
 class Card {
-  // ...
-}
+  constructor(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
 
-class Rank {
-  // ...
-}
+  get value() {
+    if (this.rank === 'A') {
+      return 11;
+    } else if (this.rank === 'K' || this.rank === 'Q' || this.rank === 'J') {
+      return 10;
+    } else {
+      return parseInt(this.rank);
+    }
+  }
 
-class Suit {
-  // ...
+  toString() {
+    return `${this.rank}${this.suit}`;
+  }
 }
 
 class Deck {
-  // ...
+  constructor() {
+    this.cards = [];
+    const suits = ['♠', '♣', '♥', '♦'];
+    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+    for (let suit of suits) {
+      for (let rank of ranks) {
+        this.cards.push(new Card(rank, suit));
+      }
+    }
+  }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+
+  drawCard() {
+    return this.cards.pop();
+  }
 }
 
 class Hand {
-  // ...
+  constructor() {
+    this.cards = [];
+  }
+
+  addCard(card) {
+    this.cards.push(card);
+  }
+
+  getScore() {
+    let score = 0;
+    let aces = 0;
+    for (let card of this.cards) {
+      score += card.value;
+      if (card.rank === 'A') {
+        aces++;
+      }
+    }
+    while (score > 21 && aces > 0) {
+      score -= 10;
+      aces--;
+    }
+    return score;
+  }
+
+  toString() {
+    return this.cards.map(card => card.toString());
+  }
 }
+
 
 function updateHandDisplay(handElement, hand) {
   handElement.innerHTML = hand.toString().replace(/,/g, '<br>');

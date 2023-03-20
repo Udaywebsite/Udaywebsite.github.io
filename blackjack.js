0c8f5f0b7b7a7d8a9a78cb3506a8a61611be1f91
Udaywebsite <128072491+Udaywebsite@users.noreply.github.com>
2023-03-20 00:51:16 -0500

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -1,25 +1,4 @@
 function openBlackjack() {
   window.open("blackjack.html", "_blank", "width=600,height=400");
 }
-document.addEventListener('DOMContentLoaded', () => {
-  const startButton = document.getElementById('start');
-  if (startButton) {
-    startButton.addEventListener('click', () => {
-      playBlackjack();
-      startButton.disabled = true;
-    });
-  }
-});
-
-
-
-function playBlackjack() {
-  const startButton = document.getElementById('start');
-
-  startButton.addEventListener('click', () => {
-    // Call the game logic function from the new "game.js" file
-    startGame();
-    startButton.disabled = true;
-  });
-}
 

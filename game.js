12688dba8e58c73335a832954a8ff2d2c846c835
Udaywebsite <128072491+Udaywebsite@users.noreply.github.com>
2024-03-07 02:23:23 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,153 +1,155 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-class Card {
-  constructor(rank, suit) {
-    this.rank = rank;
-    this.suit = suit;
-  }
-
-  get value() {
-    if (this.rank === 'A') {
-      return 11;
-    } else if (this.rank === 'K' || this.rank === 'Q' || this.rank === 'J') {
-      return 10;
-    } else {
-      return parseInt(this.rank);
+  class Card {
+    constructor(rank, suit) {
+      this.rank = rank;
+      this.suit = suit;
     }
-  }
 
-  toString() {
-    return `${this.rank}${this.suit}`;
-  }
-}
-
-class Deck {
-  constructor() {
-    this.cards = [];
-    const suits = ['♠', '♣', '♥', '♦'];
-    const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-
-    for (let suit of suits) {
-      for (let rank of ranks) {
-        this.cards.push(new Card(rank, suit));
+    get value() {
+      if (this.rank === 'A') {
+        return 11;
+      } else if (['K', 'Q', 'J'].includes(this.rank)) {
+        return 10;
+      } else {
+        return parseInt(this.rank, 10);
       }
     }
-  }
 
-  shuffle() {
-    for (let i = this.cards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    toString() {
+      return `${this.rank}${this.suit}`;
     }
   }
 
-  drawCard() {
-    return this.cards.pop();
-  }
-}
+  class Deck {
+    constructor() {
+      this.cards = [];
+      this.initialize();
+    }
 
-class Hand {
-  constructor() {
-    this.cards = [];
-  }
+    initialize() {
+      this.cards = [];
+      const suits = ['♠', '♣', '♥', '♦'];
+      const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+      for (let suit of suits) {
+        for (let rank of ranks) {
+          this.cards.push(new Card(rank, suit));
+        }
+      }
+      this.shuffle();
+    }
 
-  addCard(card) {
-    this.cards.push(card);
-  }
-
-  getScore() {
-    let score = 0;
-    let aces = 0;
-    for (let card of this.cards) {
-      score += card.value;
-      if (card.rank === 'A') {
-        aces++;
+    shuffle() {
+      for (let i = this.cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
       }
     }
-    while (score > 21 && aces > 0) {
-      score -= 10;
-      aces--;
+
+    drawCard() {
+      return this.cards.pop();
     }
-    return score;
   }
 
-  toString() {
-    return this.cards.map(card => card.toString());
+  class Hand {
+    constructor() {
+      this.cards = [];
+    }
+
+    addCard(card) {
+      this.cards.push(card);
+    }
+
+    getScore() {
+      let score = 0;
+      let aces = 0;
+      for (let card of this.cards) {
+        score += card.value;
+        if (card.rank === 'A') {
+          aces++;
+        }
+      }
+      while (score > 21 && aces > 0) {
+        score -= 10;
+        aces--;
+      }
+      return score;
+    }
+
+    toString() {
+      return this.cards.map(card => card.toString()).join('<br>');
+    }
   }
-}
 
+  // Initialize game state variables
+  let playerHand = new Hand();
+  let dealerHand = new Hand();
+  const deck = new Deck();
 
-
-function updateHandDisplay(handElement, hand) {
-  handElement.innerHTML = hand.toString().replace(/,/g, '<br>');
-}
-
-const playerHand = new Hand();
-const dealerHand = new Hand();
-const deck = new Deck(); // Move the deck variable to the global scope
-
-function startGame() {
-  deck.cards = []; // Clear deck before shuffling
-  deck.shuffle();
-
-  // Clear hands
-  playerHand.cards = [];
-  dealerHand.cards = [];
-
-  // Initial deal
-  playerHand.addCard(deck.drawCard());
-  playerHand.addCard(deck.drawCard());
-  dealerHand.addCard(deck.drawCard());
-  dealerHand.addCard(deck.drawCard());
-
-  updateHandDisplay(document.getElementById('player-hand'), playerHand);
-  updateHandDisplay(document.getElementById('dealer-hand'), dealerHand);
-
-  hitButton.disabled = false;
-  standButton.disabled = false;
-}
-
-const startButton = document.getElementById('start');
-
-startButton.addEventListener('click', () => {
-  startGame();
-  startButton.disabled = true;
-});
-
-const hitButton = document.getElementById('hit');
-const standButton = document.getElementById('stand');
-
-hitButton.addEventListener('click', () => {
-  playerHand.addCard(deck.drawCard());
-  updateHandDisplay(document.getElementById('player-hand'), playerHand);
-
-  if (playerHand.getScore() > 21) {
-    document.getElementById('result').innerHTML = 'Bust! You lose.';
-    hitButton.disabled = true;
-    standButton.disabled = true;
+  function updateHandDisplay(handElement, hand) {
+    handElement.innerHTML = hand.toString();
   }
-});
 
-standButton.addEventListener('click', () => {
-  while (dealerHand.getScore() < 17) {
+  function startGame() {
+    deck.initialize(); // Reinitialize the deck
+
+    playerHand = new Hand(); // Reset player's hand
+    dealerHand = new Hand(); // Reset dealer's hand
+
+    // Initial deal
+    playerHand.addCard(deck.drawCard());
+    playerHand.addCard(deck.drawCard());
     dealerHand.addCard(deck.drawCard());
-  }
-  updateHandDisplay(document.getElementById('dealer-hand'), dealerHand);
+    dealerHand.addCard(deck.drawCard());
 
-  const result = document.getElementById('result');
+    updateHandDisplay(document.getElementById('player-hand'), playerHand);
+    updateHandDisplay(document.getElementById('dealer-hand'), dealerHand);
 
-  if (dealerHand.getScore() > 21) {
-    result.innerHTML = "Dealer busts! You win.";
-  } else if (playerHand.getScore() > dealerHand.getScore()) {
-    result.innerHTML = "You win!";
-  } else if (playerHand.getScore() < dealerHand.getScore()) {
-    result.innerHTML = "You lose.";
-  } else {
-    result.innerHTML = "It's a tie.";
+    document.getElementById('hit').disabled = false;
+    document.getElementById('stand').disabled = false;
+    document.getElementById('result').innerHTML = ''; // Clear any previous result
+    document.getElementById('start').disabled = true; // Disable start button after game starts
   }
 
-  hitButton.disabled = true;
-  standButton.disabled = true;
-});
+  // Event listeners for game controls
+  document.getElementById('start').addEventListener('click', startGame);
+
+  document.getElementById('hit').addEventListener('click', () => {
+    playerHand.addCard(deck.drawCard());
+    updateHandDisplay(document.getElementById('player-hand'), playerHand);
+
+    if (playerHand.getScore() > 21) {
+      document.getElementById('result').innerHTML = 'Bust! You lose.';
+      document.getElementById('hit').disabled = true;
+      document.getElementById('stand').disabled = true;
+      document.getElementById('start').disabled = false; // Re-enable start button
+    }
+  });
+
+  document.getElementById('stand').addEventListener('click', () => {
+    while (dealerHand.getScore() < 17) {
+      dealerHand.addCard(deck.drawCard());
+    }
+    updateHandDisplay(document.getElementById('dealer-hand'), dealerHand);
+
+    const playerScore = playerHand.getScore();
+    const dealerScore = dealerHand.getScore();
+    let resultText = '';
+
+    if (dealerScore > 21) {
+      resultText = "Dealer busts! You win.";
+    } else if (playerScore > dealerScore) {
+      resultText = "You win!";
+    } else if (playerScore < dealerScore) {
+      resultText = "You lose.";
+    } else {
+      resultText = "It's a tie.";
+    }
+
+    document.getElementById('result').innerHTML = resultText;
+    document.getElementById('hit').disabled = true;
+    document.getElementById('stand').disabled = true;
+    document.getElementById('start').disabled = false; // Re-enable start button to allow a new game
+  });
 
 });
